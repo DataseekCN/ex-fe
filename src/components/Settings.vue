ca3da251db2c5d299003d25e790dbc7e2cc7843f
Jenks Guo <jenksguo@gmail.com>
2019-09-12 21:39:40 +1000

    Updated setting page
    80% completed

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -9,34 +9,54 @@
     <b-col class="col-auto">
       <SideBar/>
     </b-col>
-    <b-col class="col-9">
+    <b-col>
       <b-row>
-        <b-row>
-          <p>Subscription:</p>
-        </b-row>
-
-        <b-row>
+        <b-col sm="4" class="my-3">
+          <p>Current Subscription:</p>
           <div class="card">
             <div class="card-body">
               <h4 class="card-title">Title</h4>
               <p class="card-text">Body</p>
             </div>
           </div>
-        </b-row>
+        </b-col>
+      </b-row>
 
-        <b-row>
+      <b-row>
+        <b-container fluid>
           <p>Profile:</p>
-        </b-row>
+          <b-row class="my-1" v-for="(type, label) in form_inputs" :key="label">
+            <b-col sm="2">
+              <label :for="`type-${type}`">{{ label }}:</label>
+            </b-col>
+            <b-col sm="6">
+              <b-form-input :id="`type-${type}`" :type="type" v-model="form_outputs[label]"></b-form-input>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-row>
 
-        <b-row>
-          <div class="form-group">
-            <label for=""></label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-            <small id="helpId" class="form-text text-muted">Help text</small>
-          </div>
-          <button type="button" name="" id="" class="btn btn-primary btn-lg btn-block">Save</button>
+      <b-row>
+        <b-container fluid>
+          <p>Reset Passwords:</p>
+          <b-row class="my-1" v-for="(type, label) in newpasswords_inputs" :key="label">
+            <b-col sm="2">
+              <label :for="`type-${type}`">{{ label }}:</label>
+            </b-col>
+            <b-col sm="6">
+              <b-form-input :id="`type-${type}`" :type="type" v-model="newpasswords_outputs[label]"></b-form-input>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-row>
+
+      <b-row class="m-1">
+        <b-col class="m-1">
+          <button type="button" class="btn btn-primary btn-lg btn-block" v-on:click="output">Save</button>
+        </b-col>
+        <b-col class="m-1">
           <button type="button" name="" id="" class="btn btn-primary btn-lg btn-block">Cancel</button>
-        </b-row>
+        </b-col>
       </b-row>
     </b-col>
   </b-row>
@@ -55,10 +75,44 @@ export default {
   },
   data() {
     return {
+      form_inputs: {
+        Name: 'text',
+        Company: 'text',
+        Country: 'text',
+        Email: 'email',
+      },
+      form_outputs: {
+        Name: null,
+        Company: null,
+        Country: null,
+        Email: null,
+      },
+      newpasswords_inputs: {
+        Old: 'password',
+        New: 'password',
+        Confirm: 'password',
+      },
+      newpasswords_outputs: {
+        Old: null,
+        New: null,
+        Confirm: null,
+      },
     };
   },
   created() {
 
+  },
+  methods: {
+    output(event) {
+      if (event) {
+        // eslint-disable-next-line
+        console.log(this.form_outputs)
+        // eslint-disable-next-line
+        console.log('--------------------')
+        // eslint-disable-next-line
+        console.log(this.newpasswords_outputs)
+      }
+    },
   },
 };
 </script>
